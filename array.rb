9f0821a89e70680901f5d7ada32c53fbec7eddc0
Midori-Gift <okometaberoyo5009@gmail.com>
2021-08-24 06:42:27 +0000

    5章復習

---
+++ array.rb
@@ -0,0 +1,8 @@
+names = ["Git", "HTML", "CSS"]
+
+puts names
+
+puts"---配列で個別に呼び出し--"
+puts names[0]
+puts names[1]
+puts names[2]
---
+++ weight.rb
@@ -0,0 +1,14 @@
+name = "A"
+
+weight = 50
+
+puts name + "さんの体重は" + weight.to_s + "kgです"
+
+puts "#{name}さんの体重は#{weight}kgです"
+
+puts "#{name}さんの体重は#{weight}kgです"
+
+puts "#{name}さんの体重は#{weight}kgです"
+
+# 変数展開#{}　を使うときは""ダブルクォーテーションを使わなければならない！
+puts '#{name}さんの体重は#{weight}kgです'
